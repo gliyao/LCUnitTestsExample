2b5e63a0e8ae368756a30649938994f06405da8b
Liyao Chen <gliyao@gmail.com>
2017-04-08 14:28:45 +0800

    Feat: Add stub example for UITableViewDataSource

--- LCUnitTestsExampleTests/LCTableViewTests.m
+++ LCUnitTestsExampleTests/LCTableViewTests.m
@@ -8,7 +8,24 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
-#import "DummyUITableViewDataSource.h"
+#import "OCMock.h"
+
+@interface DummyUITableViewDataSource : NSObject <UITableViewDataSource>
+@end
+@implementation DummyUITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 40;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+@end
+
 
 @interface LCTableViewTests : XCTestCase
 
@@ -17,12 +34,44 @@
 @implementation LCTableViewTests
 
 //驗證互動
-- (void)testExample
+- (void)testTableViewDataSourceNumberOfRowsInSecitonWithDummyClass
 {
     //Arrange
     DummyUITableViewDataSource *dataSource = [[DummyUITableViewDataSource alloc] init];
     UITableView *tableView = [[UITableView alloc] init];
     tableView.dataSource = dataSource;
+    
+    //Act
+    [tableView reloadData];
+    
+    //Assert
+    NSInteger rowCount = [tableView numberOfRowsInSection:0];
+    XCTAssertEqual(rowCount, 40);
+}
+
+
+- (void)testOCMockStubMethod
+{
+    //Arrange
+    id protocolMock = OCMProtocolMock(@protocol(UITableViewDataSource));
+    OCMStub([protocolMock numberOfSectionsInTableView:OCMOCK_ANY]).andReturn(1);
+    OCMStub([protocolMock tableView:OCMOCK_ANY numberOfRowsInSection:0]).andReturn(40);
+    OCMStub([protocolMock tableView:OCMOCK_ANY cellForRowAtIndexPath:OCMOCK_ANY]).andReturn(nil);
+    
+    XCTAssertEqual([protocolMock numberOfSectionsInTableView:OCMOCK_ANY], 1);
+    XCTAssertEqual([protocolMock tableView:OCMOCK_ANY numberOfRowsInSection:0], 40);
+    XCTAssertNil([protocolMock tableView:OCMOCK_ANY cellForRowAtIndexPath:OCMOCK_ANY]);
+}
+
+- (void)testTableViewDataSourceNumberOfRowsInSecitonWithStub
+{
+    //Arrange
+    id protocolMock = OCMProtocolMock(@protocol(UITableViewDataSource));
+    OCMStub([protocolMock numberOfSectionsInTableView:OCMOCK_ANY]).andReturn(1);
+    OCMStub([protocolMock tableView:OCMOCK_ANY numberOfRowsInSection:0]).andReturn(40);
+    OCMStub([protocolMock tableView:OCMOCK_ANY cellForRowAtIndexPath:OCMOCK_ANY]).andReturn(nil);
+    UITableView *tableView = [[UITableView alloc] init];
+    tableView.dataSource = protocolMock;
     
     //Act
     [tableView reloadData];
